1eb4b0cf077bea3290c772565387447e3214a0fe
anderson-bassan <bassan.programador@gmail.com>
2022-08-26 11:21:45 -0300

    add elements to the grid

--- src/main/java/org/example/MyFrame.java
+++ src/main/java/org/example/MyFrame.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
     public MyFrame() {
         this.set();
+        this.addElements();
     }
 
     public void set() {
@@ -12,6 +14,14 @@ public class MyFrame extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
         this.setTitle("Bag Grid Layout");
+        this.setLayout(new GridBagLayout());
+    }
+
+    public void addElements() {
+        this.add(new JLabel("Hello, friend!"));
+        this.add(new JButton("Are you real?"));
+        this.add(new JLabel("A bug is never just a mistake. It represents something bigger."));
+        this.add(new JButton("An error of thinking who makes who you are"));
     }
 
     public void run() {
