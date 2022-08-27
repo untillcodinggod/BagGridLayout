8167c7fedadb310f95e95412e52302de24043aba
anderson-bassan <bassan.programador@gmail.com>
2022-08-26 21:05:38 -0300

    implements gridbaglayout in project

--- src/main/java/org/example/MyFrame.java
+++ src/main/java/org/example/MyFrame.java
@@ -4,7 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyFrame extends JFrame {
+    private final GridBagConstraints constraints;
+
     public MyFrame() {
+        this.constraints = new GridBagConstraints();
+
         this.set();
         this.addElements();
     }
@@ -17,11 +21,72 @@ public class MyFrame extends JFrame {
         this.setLayout(new GridBagLayout());
     }
 
+    public void setFirstLabelConstraints() {
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+    }
+
+    public void setFirstButtonConstraints() {
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+    }
+
+    public void setSecondButtonConstraints() {
+        constraints.gridx = 1;
+        constraints.gridy = 3;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+    }
+
+    public void setSecondLabelConstraints() {
+        constraints.gridx = 1;
+        constraints.gridy = 4;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+    }
+
+    public void setSpacerConstraints() {
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+    }
+
+    public void addFirstLabel() {
+        setFirstLabelConstraints();
+        this.add(new JLabel("Hello, friend!"), constraints);
+    }
+
+    public void addFirstButton() {
+        setFirstButtonConstraints();
+        this.add(new JButton("Are you real?"), constraints);
+    }
+
+    public void addSecondLabel() {
+        setSecondLabelConstraints();
+        this.add(new JLabel("A bug is never just a mistake. It represents something bigger."), constraints);
+    }
+
+    public void addSecondButton() {
+        setSecondButtonConstraints();
+        this.add(new JButton("An error of thinking who makes who you are"), constraints);
+    }
+
+    public void addSpacer() {
+        setSpacerConstraints();
+        this.add(new JPanel(), constraints);
+    }
+
     public void addElements() {
-        this.add(new JLabel("Hello, friend!"));
-        this.add(new JButton("Are you real?"));
-        this.add(new JLabel("A bug is never just a mistake. It represents something bigger."));
-        this.add(new JButton("An error of thinking who makes who you are"));
+        addFirstLabel();
+        addFirstButton();
+        addSpacer();
+        addSecondLabel();
+        addSecondButton();
     }
 
     public void run() {
